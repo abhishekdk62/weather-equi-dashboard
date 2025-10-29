1617a73ec6ce08f123d9f37194c1554444136e31
ABHISHEK <abhidk62@gmail.com>
2025-10-30 00:17:57 +0530

    added env example file and readme

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const apiRoutes = require('./src/routes/api');
-const connectDB = require('./src/config/database');
+const apiRoutes = require("./src/routes/api");
+const connectDB = require("./src/config/database");
 
 const app = express();
-  
-app.use(cors({origin:process.env.NODE_ENV=='dev'?process.env.DEV_URL:process.env.PROD_URL}));
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV == "dev"
+        ? process.env.DEV_URL
+        : process.env.PROD_URL,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'OK', message: 'Server is running' });
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK", message: "Server is running" });
 });
 
 const PORT = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-app.listen(PORT,()=>{
-    console.log(`server is listening on http://localhost:${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`server is listening on http://localhost:${PORT}`);
+});
 
 module.exports = app;
